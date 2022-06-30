3035bb2958e4b24bb4b744c8c965e326bd9eb5ba
Sotatek-DucPham <duc.pham@sotatek.com>
2022-06-30 10:40:11 +0700

    feat: structure

--- modules/evm-bridge/src/lib.rs
+++ modules/evm-bridge/src/lib.rs
@@ -1,1 +1,47 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{
+	ensure,
+	pallet_prelude::*,
+	traits::{Currency, IsType, OnKilledAccount},
+	transactional,
+};
+use frame_system::{ensure_signed, pallet_prelude::*};
+
+// pub use module::*;
+
+#[frame_support::pallet]
+pub mod module {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+	}
+
+	// #[pallet::event]
+	// #[pallet::generate_deposit(pub(crate) fn deposit_event)]
+	// pub enum Event<T: Config> {
+	// }
+
+	/// Error for evm accounts module.
+	#[pallet::error]
+	pub enum Error<T> {
+	}
+
+	/// The Substrate Account for EvmAddresses
+	///
+	/// Accounts: map EvmAddress => Option<AccountId>
+	// #[pallet::storage]
+	// #[pallet::getter(fn accounts)]
+	// pub type Accounts<T: Config> = StorageMap<_, Twox64Concat, EvmAddress, T::AccountId, OptionQuery>;
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+	}
+}
